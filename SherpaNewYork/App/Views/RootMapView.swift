a552660d6634cd7d77e0bdbb39bff4c8cd4a8cc6
Edmund Mai <edmundm@crowdtap.com>
2015-10-24 17:32:28 -0400

    add filters button

--- SherpaNewYork/App/Views/RootMapView.swift
+++ SherpaNewYork/App/Views/RootMapView.swift
@@ -3,11 +3,27 @@ import GoogleMaps
 
 class RootMapView: UIView {
   
+  var coordinates: CLLocationCoordinate2D
   var map: GMSMapView?
 
   init(frame: CGRect, coordinates: CLLocationCoordinate2D, zoom: Float) {
-    super.init(frame: frame)
+    self.coordinates = coordinates
     
+    super.init(frame: frame)
+
+    addMap()
+    addFilters()
+  }
+
+  required init(coder aDecoder: NSCoder) {
+    fatalError("Don't use Storyboards, newb!")
+  }
+  
+  func showFilters(sender: UIButton!) {
+    println("Filter clicked")
+  }
+  
+  private func addMap() {
     let camera = GMSCameraPosition.cameraWithLatitude(
       coordinates.latitude, longitude:
       coordinates.longitude,
@@ -17,9 +33,14 @@ class RootMapView: UIView {
     addMarkers(coordinates)
     addSubview(map!)
   }
-
-  required init(coder aDecoder: NSCoder) {
-    fatalError("Don't use Storyboards, newb!")
+  
+  private func addFilters() {
+    let filterButton = UIButton()
+    let filterImage = UIImage(named: "filter-1")
+    filterButton.frame = CGRectMake(frame.size.width-100, 30, 70, 70)
+    filterButton.setImage(filterImage, forState: UIControlState.Normal)
+    filterButton.addTarget(self, action: "showFilters:", forControlEvents: UIControlEvents.TouchUpInside)
+    addSubview(filterButton)
   }
   
   private func addMarkers(coordinates: CLLocationCoordinate2D) {
@@ -29,35 +50,40 @@ class RootMapView: UIView {
     
     // make request to server that returns a bunch of places
     let jsonResponse: [Dictionary<String, AnyObject>] = [
-      ["id": "1",
+      [
+        "id": "1",
         "name": "Crumbs Bakery",
         "description": "Oven baked cookies and cupcakes",
         "icon_image": "cafe_pin",
         "latitude": 40.714,
         "longitude": -74.000
       ],
-      ["id": "2",
+      [
+        "id": "2",
         "name": "Shake Shack",
         "description": "Best burgers and french fries",
         "icon_image": "restaurant_pin",
         "latitude": 40.715,
         "longitude": -74.001
       ],
-      ["id": "3",
+      [
+        "id": "3",
         "name": "Whole Foods",
         "description": "Overpriced organic stuff",
         "icon_image": "grocery_or_supermarket_pin",
         "latitude": 40.716,
         "longitude": -74.000
       ],
-      ["id": "4",
+      [
+        "id": "4",
         "name": "Peter Luger",
         "description": "Big pieces of steak",
         "icon_image": "restaurant_pin",
         "latitude": 40.713,
         "longitude": -73.999
       ],
-      ["id": "5",
+      [
+        "id": "5",
         "name": "Century 21",
         "description": "Brand names for half the price",
         "icon_image": "driving_pin",
