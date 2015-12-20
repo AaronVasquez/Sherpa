8783c735ee69bc73a8474185ebbba3c899e02eed
Aaron Vasquez <aaronvasquez1@gmail.com>
2015-12-20 00:17:47 -0500

    first pass at filtering

--- SherpaNewYork/App/Controllers/RootMapViewController.swift
+++ SherpaNewYork/App/Controllers/RootMapViewController.swift
@@ -11,31 +11,54 @@ private let kPinIconRestaurant = "restaurant_pin"
 private let kPinIconGrocery = "grocery_pin"
 private let kPinIconDriving = "driving_pin"
 
+private let kShowFilterSegue = "showVenueFilterViewController"
+
 class RootMapViewController: UIViewController {
 
   let locationManager = CLLocationManager()
-  let venues: [Venue] = VenueRepository.fetchVenues()
+  let allVenues: [Venue] = VenueRepository.fetchVenues()
 
   @IBOutlet weak var mapView: GMSMapView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    tabBarController?.tabBar.hidden = false
     mapView.delegate = self
 
     initialLocationSetup()
     fetchLocation()
 
-    for venue in venues {
-      self.addMapPin(venue)
+    reloadMap(nil)
+  }
+
+  @IBAction func showFilterViewController(sender: AnyObject) {
+    performSegueWithIdentifier(kShowFilterSegue, sender: self)
+  }
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if (segue.identifier == kShowFilterSegue) {
+      let venueFilterVc = segue.destinationViewController as! VenueFilterViewController
+      venueFilterVc.venueFilter = VenueFilter.init()
+      venueFilterVc.filterDelegate = self
     }
   }
-  
-  override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(animated)
-    tabBarController?.tabBar.hidden = false
+
+  private func reloadMap(filter: VenueFilter?) {
+    mapView.clear()
+
+    allVenues.filter({ venue -> Bool in
+      if let filter = filter {
+        if let type = filter.type {
+          return type == venue.type
+        }
+      }
+      return true;
+    }).forEach({
+      self.addMapPin($0)
+    })
   }
-  
-  func addMapPin(venue: Venue) {
+
+  private func addMapPin(venue: Venue) {
     let mapPin = GMSMarker(position: venue.coordinates)
     mapPin.map = mapView
     mapPin.title = venue.name
@@ -99,4 +122,10 @@ extension RootMapViewController: GMSMapViewDelegate {
     }
   }
   
+}
+
+extension RootMapViewController: VenueFilterDelegate {
+  func filterDidChange(filter: VenueFilter) {
+    reloadMap(filter)
+  }
 }
