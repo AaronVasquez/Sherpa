f3a0623259492ed71855c398f2b42abc5c48d812
Aaron Vasquez <aaronvasquez1@gmail.com>
2016-01-31 21:02:37 -0500

    forgot to add

---
+++ SherpaNewYork/App/Views/VenueTableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+public class VenueTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var bannerImageView: UIImageView!
+  @IBOutlet weak var informationContainerView: UIView!
+  @IBOutlet weak var titleLabel: UILabel!
+
+}
