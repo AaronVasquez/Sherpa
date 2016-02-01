7072bd2f2916050048ab759fa720e71e43ad3962
Aaron Vasquez <aaronvasquez1@gmail.com>
2016-01-31 21:02:28 -0500

    synx

--- SherpaNewYork/App/VenueTableViewCell.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-public class VenueTableViewCell: UITableViewCell {
-
-  @IBOutlet weak var bannerImageView: UIImageView!
-  @IBOutlet weak var informationContainerView: UIView!
-  @IBOutlet weak var titleLabel: UILabel!
-
-}
