6368150dbcea76acc093ab4ed1da9b203e55e875
Aaron Vasquez <aaronvasquez1@gmail.com>
2016-02-21 15:24:44 -0500

    switch to a toggle

--- SherpaNewYork/App/Controllers/RootViewController.swift
+++ SherpaNewYork/App/Controllers/RootViewController.swift
@@ -22,12 +22,11 @@ class RootViewController: UIViewController {
   
   let locationManager = CLLocationManager()
   let venueCollection = VenueCollection.init();
+
   var chosenVenue: Venue?
   var venueFilter = VenueFilter.init()
-
   var listViewController: VenueListViewController?
   var mapViewController: MapViewController?
-  var listViewControllerShown = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -38,23 +37,18 @@ class RootViewController: UIViewController {
     performSegueWithIdentifier(kShowFilterSegue, sender: mapViewController!)
   }
 
-  @IBAction func showListViewController(sender: AnyObject) {
-    if (listViewControllerShown) {
-      UIView.transitionFromView(listViewController!.view,
-          toView: mapViewController!.view,
-          duration: 0.2,
-          options: UIViewAnimationOptions.TransitionCrossDissolve,
-          completion: nil)
-    } else {
-      UIView.transitionFromView(mapViewController!.view,
-          toView: listViewController!.view,
-          duration: 0.2,
-          options: UIViewAnimationOptions.TransitionCrossDissolve,
-          completion: nil)
-    }
-    listViewControllerShown = !listViewControllerShown
+  @IBAction func toggleMapAndList(sender: UISegmentedControl) {
+    let showMap = sender.selectedSegmentIndex == 0
+    let fromView =  showMap ? listViewController!.view : mapViewController!.view
+    let toView = showMap ? mapViewController!.view : listViewController!.view
+
+    UIView.transitionFromView(fromView,
+      toView: toView,
+      duration: 0.2,
+      options: UIViewAnimationOptions.TransitionCrossDissolve,
+      completion: nil)
   }
-  
+
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if (segue.identifier == kShowFilterSegue) {
       //VenueFilterViewController
@@ -63,21 +57,22 @@ class RootViewController: UIViewController {
       venueFilterVc.venueFilter = self.venueFilter
       venueFilterVc.filterDelegate = self
     }
-    if (segue.identifier == kMapEmbedSegue) {
+
+    else if (segue.identifier == kMapEmbedSegue) {
       let mapVc = segue.destinationViewController as! MapViewController
       mapVc.delegate = self
       mapVc.venueCollection = self.venueCollection
       mapViewController = mapVc
     }
     
-    if (segue.identifier == kListEmbedSegue) {
+    else if (segue.identifier == kListEmbedSegue) {
       let listVc = segue.destinationViewController as! VenueListViewController
       listVc.delegate = self
       listVc.venueCollection = self.venueCollection
       listViewController = listVc
     }
     
-    if (segue.identifier == kVenueDetailSegue) {
+    else if (segue.identifier == kVenueDetailSegue) {
       let venueDetailVc = segue.destinationViewController as! VenueDetailViewController
       venueDetailVc.venue = chosenVenue
     }
