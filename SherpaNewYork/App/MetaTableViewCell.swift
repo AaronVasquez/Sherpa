7d2d0e695d86f902909a1670b67ffbf37862ae0c
Edmund Mai <edmundmai@Edmunds-MacBook-Air.local>
2016-03-13 14:55:01 -0400

    changes stack into table wip

---
+++ SherpaNewYork/App/MetaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MetaTableViewCell.swift
+//  SherpaNewYork
+//
+//  Created by Edmund Mai on 3/13/16.
+//  Copyright © 2016 Aaron Vasquez. All rights reserved.
+//
+
+import UIKit
+
+class MetaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
